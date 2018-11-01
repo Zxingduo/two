380d88f334bfd70ac38d41bffbb46907928f666a
Zxingduo <1209611847@qq.com>
2018-11-01 22:44:12 +0800

    完成验证码注册

--- app/Http/Controllers/DemoController.php
+++ app/Http/Controllers/DemoController.php
@@ -7,11 +7,47 @@ use Illuminate\Support\Facades\Route;
 
 class DemoController extends Controller
 {
-    public function index(){
+    public function index()
+    {
+        $message = 'hello';
+
+//// 没有 "use"
+//        $example = function () {
+//            var_dump($message);
+//        };
+//        echo $example();
+
+// 继承 $message
+        $example = function () use ($message) {
+            var_dump($message);
+        };
+        echo $example();
+
+// Inherited variable's value is from when the function
+// is defined, not when called
+        $message = 'world';
+        echo $example();
+
+// Reset message
+        $message = 'hello';
+
+// Inherit by-reference
+        $example = function () use (&$message) {
+            var_dump($message);
+        };
+        echo $example();
+
+// The changed value in the parent scope
+// is reflected inside the function call
+        $message = 'world';
+        echo $example();
+
+// Closures can also accept regular arguments
+        $example = function ($arg) use ($message) {
+            var_dump($arg . ' ' . $message);
+        };
+        $example("hello");
 
 
-        $value = Route::currentRouteName();
-
-        return $value;
     }
 }
